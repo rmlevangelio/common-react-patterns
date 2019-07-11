752020cd4a39ac6020a44c27e4b0d5b18ade2e7b
Remiel Evangelio <rmlevangelio@gmail.com>
2019-07-11 12:07:24 +0800

    master * Initial commit * Dynamic composition and Higher order component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import * as React from 'react';
+import { MainView } from './MainView/MainView';
+import { columns } from './MainView/constants';
+import { map, identity } from 'lodash/fp';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const usersData = {
+  '0001': {firstName: 'comfort', lastName: 'account', age: 27, status: 'relationshipnail', visits: 59},
+  '0002': {firstName: 'tin', lastName: 'throne', age: 20, status: 'relationship', visits: 53},
+  '0003': {firstName: 'trains', lastName: 'highway', age: 29, status: 'hand', visits: 2},
+  '0004': {firstName: 'difficulty', lastName: 'clock', age: 28, status: 'single', visits: 89},
+  '0005': {firstName: 'window', lastName: 'honey', age: 29, status: 'complicated', visits: 7},
+}
+
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      tags: []
+    }
+  }
+
+  updateTags = (tags) => {
+    this.setState({tags});
+  }
+
+  render() {
+    const dataArr = map(identity, usersData);
+    return (
+      <div>
+        <MainView
+          color='#ff0000'
+          columns={columns}
+          data={dataArr}
+          tags={this.state.tags}
+          updateTags={this.updateTags}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/MainView/MainView.js
@@ -0,0 +1,99 @@
+import * as React from 'react';
+import ReactTable from 'react-table';
+import TagsInput from 'react-tagsinput';
+
+import 'react-table/react-table.css'
+import 'react-tagsinput/react-tagsinput.css'
+import './MainView.css';
+
+// Dynamic composition: The composition is created when the render method is executed
+// Total isolation between wrapping behavior and whats inside. No communication
+class WithBorder extends React.Component {
+  constructor(props) {
+    super();
+  
+    this.state = {
+      borderColor: props.color
+    }
+  }
+  
+  onMouseEnter = () => {
+    this.setState({ borderColor: '#000' });
+  }
+  
+  onMouseLeave = () => {
+    this.setState({ borderColor: this.props.color });
+  }
+  
+  render() {
+    return (
+      <div
+        className='withBorder'
+        onMouseEnter={this.onMouseEnter}
+        onMouseLeave={this.onMouseLeave}
+        style={{ borderColor: this.state.borderColor }}
+      >
+        {this.props.children}
+      </div>
+    );
+  }
+}
+
+// Static Composition: The composition is now created during class creation time and not the render time
+// Repackage into a new Component
+// const TableWithBorder = props => (
+//   <WithBorder color={props.color}>
+//     <ReactTable 
+//       columns={props.columns}
+//       data={props.data}
+//       defaultPageSize={5}
+//     />
+//   </WithBorder>
+// )
+
+// const TagsWithBorder = props => (
+//   <WithBorder color={props.color}>
+//     <TagsInput value={props.value} onChange={props.onChange}/>
+//   </WithBorder>
+// )
+
+
+// HOC
+// Props collision is the problem. Change name to reduce collisions
+const withBorderHOC = (InnerComp) => (props) => (
+  <WithBorder color={props.color}>
+    <InnerComp {...props} />
+  </WithBorder>
+)
+
+const TableWithBorder = withBorderHOC(ReactTable);
+const TagsWithBorder = withBorderHOC(TagsInput);
+
+
+export class MainView extends React.Component {
+  updateTags = newTags => {
+    this.props.updateTags(newTags);
+  }
+
+  render() {
+    return (
+      <div className='mainContainer'>
+        <div className='tableContainer'>
+          <TableWithBorder
+            color={this.props.color}
+            columns={this.props.columns}
+            data={this.props.data}
+            defaultPageSize={5}
+          />
+        </div>
+        <div className='tagsContainer'>
+          <TagsWithBorder
+            color={this.props.color}
+            value={this.props.tags}
+            onChange={this.updateTags}
+          />
+        </div>
+      </div>
+    );
+  }
+}
